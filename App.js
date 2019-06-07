a65bd3d44395241506e055a24647c4ef99812166
Raphael Onofre <raphael.onofre@gmail.com>
2019-06-07 10:59:51 -0300

    fix: update colos

--- App.js
+++ App.js
@@ -1,66 +1,95 @@
-import React, { Component } from "react";
-import { View, Text, Platform, StatusBar } from "react-native";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import {
-  createBottomTabNavigator,
-  createStackNavigator
-} from "react-navigation";
-import reducers from "./reducers";
-import { purple, white } from "./util/colors";
-import { Constants } from "expo";
+import React, { Component } from 'react'
+import { View, Text, Platform, StatusBar } from 'react-native'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers'
+import { Constants } from 'expo'
 import { FontAwesome, Feather, SimpleLineIcons } from '@expo/vector-icons'
-import { setLocalNotification } from "./util/notification";
-import DeckList from "./components/DeckList";
-import NewDeck from "./components/NewDeck";
-// import DeckDetails from './components/DeckDetails'
-import * as Colors from "./util/colors";
+import { setLocalNotification } from './util/notification'
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation'
+import DeckList from './components/DeckList'
+import NewDeck from './components/NewDeck'
+import Deck from './components/Deck'
+import * as Colors from './util/colors'
 
-const Tabs = createBottomTabNavigator({
-  Decks: {
-    screen: DeckList,
-    navigationOptions: {
-      tabBarLabel: 'Decks',
-      tabBarIcon: ({ tintColor }) => (
-          <SimpleLineIcons name='layers' size={30} color={tintColor} />
-      ),
+const Tabs = createBottomTabNavigator (
+    {
+      Decks: {
+        screen: DeckList,
+        navigationOptions: {
+          tabBarLabel: 'Decks',
+          tabBarIcon: ({ tintColor }) => (
+              <SimpleLineIcons name='layers' size={30} color={tintColor} />
+          ),
+        },
+      },
+      NewDeck: {
+        screen: NewDeck,
+        navigationOptions: {
+          tabBarLabel: 'New Deck',
+          tabBarIcon: ({ tintColor }) => (
+              <Feather name='plus-square' size={30} color={tintColor} />
+          ),
+        },
+      },
     },
-  },
-  NewDeck: {
-    screen: NewDeck,
-    navigationOptions: {
-      tabBarLabel: 'New Deck',
-      tabBarIcon: ({ tintColor }) => (
-          <Feather name='plus-square' size={30} color={tintColor} />
-      ),
-    },
-  }
-});
+    {
+      initialRouteName: 'Decks',
+      navigationOptions: {
+        header: null,
+      },
+      tabBarOptions: {
+        inactiveTintColor: Colors.gray,
+        activeTintColor: Colors.purple,
+        style: {
+          height: 56,
+          backgroundColor: Colors.darkBlue
+        }
+      }
+    }
+)
+
+const MainNavigator = createStackNavigator(
+    {
+      Home: {
+        screen: Tabs
+      },
+      Deck: {
+        screen: Deck
+      },
+    }, {
+      navigationOptions: {
+        headerTintColor: Colors.white,
+        headerStyle: {
+          backgroundColor: Colors.gray,
+          height: 40,
+          paddingTop: 0
+        },
+        headerTitleStyle: {
+          alignSelf: 'center',
+          textAlign: 'center'
+        },
+        headerRight: (<View></View>)
+      }
+    }
+);
+
 
 export default class App extends React.Component {
   componentDidMount() {
-    setLocalNotification();
+    setLocalNotification()
   }
 
   render() {
     return (
-      <Provider store={createStore(reducers)}>
-        <View style={{ flex: 1 }}>
-          <View
-            style={{
-              backgroundColor: purple,
-              height: Constants.statusBarHeight
-            }}
-          >
-            <StatusBar
-              translucent
-              backgroundColor={purple}
-              barStyle="light-content"
-            />
+        <Provider store={createStore(reducers)}>
+          <View style={{ flex: 1 }} >
+            <View style={{ backgroundColor: Colors.purple, height: Constants.statusBarHeight }}>
+              <StatusBar translucent backgroundColor={Colors.purple} barStyle='light-content' />
+            </View>
+            <MainNavigator />
           </View>
-          <Tabs />
-        </View>
-      </Provider>
-    );
+        </Provider>
+    )
   }
 }
--- util/colors.js
+++ util/colors.js
@@ -1,6 +1,9 @@
-export const black = "#000000";
-export const lightGray = "#D3D3D3";
-export const gray = "#737373";
-export const white = "#fff";
-export const purple = "#292477";
-export const lightPurple = "#7c53c3";
+export const purple = '#292477'
+export const gray = '#757575'
+export const white = '#fff'
+export const red = '#b71845'
+export const orange = '#f26f28'
+export const blue = '#4e4cb8'
+export const lightPurp = '#7c53c3'
+export const pink = '#b93fb3'
+export const black = '#000000'
