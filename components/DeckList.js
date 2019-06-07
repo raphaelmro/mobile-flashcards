422afaa3051661870c7c791af30ffdf7c006f622
Raphael Onofre <raphael.onofre@gmail.com>
2019-06-07 11:20:50 -0300

    fix: put some style on decklist

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { connect } from "react-redux";
-import { AppLoading } from "expo";
 import { receiveDecks } from "../actions";
 import { fetchFlashCards } from "../util/api";
+import * as Colors from "../util/colors";
 
 class DeckList extends Component {
   state = {
@@ -25,26 +25,49 @@ class DeckList extends Component {
     const { decks } = this.props;
 
     if (decks === "undefined") {
-      return <AppLoading />;
+      return <Text>Add a new deck.</Text>;
     }
 
     return (
-      <View>
+      <View style={{ flex: 1, alignItems: "center" }}>
         {Object.keys(decks).map(deck => (
-          <View key={deck}>
-            <Text>{decks[deck].title}</Text>
+          <TouchableOpacity style={styles.container} key={deck}>
+            <Text style={{ fontSize: 20, color: Colors.white }}>
+              {decks[deck].title}
+            </Text>
             {decks[deck].questions.length > 1 ? (
-              <Text>{decks[deck].questions.length} cards</Text>
+              <Text style={{ fontSize: 20, color: Colors.black }}>
+                {decks[deck].questions.length} cards
+              </Text>
             ) : (
-              <Text>{decks[deck].questions.length} card</Text>
+              <Text style={{ fontSize: 20, color: Colors.black }}>
+                {decks[deck].questions.length} card
+              </Text>
             )}
-          </View>
+          </TouchableOpacity>
         ))}
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.lightPurp,
+    width: 200,
+    alignItems: "center",
+    borderRadius: 16,
+    padding: 20,
+    marginTop: 17,
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: Colors.gray,
+    shadowOffset: {
+      width: 0,
+      height: 3
+    }
+  }
+});
 const mapStateToProps = state => {
   return {
     decks: state
