80874f78e5f1261d0aa3d3805364742ad6a86189
Raphael Onofre <raphael.onofre@gmail.com>
2019-06-07 10:13:15 -0300

    feat: create NewDeck component

---
+++ components/NewDeck.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import {
+  View,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from "react-native";
+import { connect } from "react-redux";
+import { addDeck } from "../actions";
+import * as Colors from "../util/colors";
+
+class NewDeck extends Component {
+  state = {
+    text: ""
+  };
+
+  static navigationOptions = ({ navigation }) => ({
+    headerTitle: "New Deck"
+  });
+
+  submit = () => {
+    const entry = this.state.text;
+    const { decks, addDeck, navigation } = this.props;
+    const input = entry.trim();
+    addDeck({ title: input });
+    this.setState({ text: "" });
+  };
+
+  render() {
+    const { text } = this.state;
+    return (
+      <KeyboardAvoidingView behavior="padding">
+        <Text>New Deck Name</Text>
+
+        <View>
+          <TextInput
+            style={{ backgroundColor: "pink", width: 300, height: 50 }}
+            underlineColor="green"
+            value={text}
+            onChangeText={text => this.setState({ text })}
+          />
+        </View>
+        <TouchableOpacity onPress={this.submit}>
+          <Text>Submit</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    decks: state
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { addDeck }
+)(NewDeck);
